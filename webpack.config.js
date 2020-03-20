29d41beb5cafb102df9f704089bb92c883aa34bf
mukil <malte@mikromedia.de>
2020-03-20 09:18:07 +0100

    Updated webpack conf to resolve vue components (#1)

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const PLUGIN_URI = 'systems.dmx.upload-dialog'
+
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { VueLoaderPlugin }  = require('vue-loader')
 
 module.exports = {
   entry: './src/main/js/plugin.js',
@@ -11,8 +14,29 @@ module.exports = {
     library: '_' + PLUGIN_URI.replace(/[.-]/g, '_'),
     libraryTarget: 'jsonp'
   },
+  resolve: {
+    extensions: [".js", ".vue"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin(),
+    new VueLoaderPlugin()
   ],
   stats: {
     entrypoints: false
