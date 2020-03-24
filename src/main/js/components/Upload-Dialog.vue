a4bad706890357ce290158a2e1e2f5d5e2e64d04
mukil <malte@mikromedia.de>
2020-03-24 20:36:42 +0100

    Upload dialog utilizes custom handler for the first time (#1)

--- src/main/js/components/Upload-Dialog.vue
+++ src/main/js/components/Upload-Dialog.vue
@@ -2,19 +2,27 @@
   <div class="dm5-upload-command">
     <el-button type="text" class="fa fa-upload" title="Upload File to DMX" @click="openUploadDialog"></el-button>
     <div v-if="uploadDialogVisible" class="dm5-upload-dialog">
-      <el-upload class="manual-upload"
-          ref="upload" :action="uploadPath"
-          with-credentials multiple
-          :on-change="dialogChanged"
-          :on-error="uploadError"
-          :on-success="uploadSuccess"
-          :auto-upload="false">
-          <div class="el-upload__tip" slot="tip"><!-- Todo: Let other plugins hook in their component here --></div>
-          <el-button slot="trigger" size="small" type="primary">Select file</el-button>
-          <el-button style="margin-left: 10px;" size="small" type="success"
-            @click="submitUpload">Upload to server</el-button>
-          <el-button size="small" type="danger" @click="closeUploadDialog">Cancel</el-button>
-      </el-upload>
+        <el-upload class="manual-upload"
+            ref="upload" :action="uploadPath"
+            with-credentials
+            :on-change="selectionHandler"
+            :on-error="errorHandler"
+            :on-success="successHandler"
+            :before-upload="beforeUpload"
+            :auto-upload="false" :list-type="fileListType">
+            <!--div slot="file" slot-scope="{file}">
+              <span>{{file.name}}</span>
+              <span class="el-upload-list__item-actions">
+                <span class="el-upload-list__item-delete" @click="handleRemove(file)">
+                  <i class="el-icon-delete"></i>
+                </span>
+              </span>
+            </div-->
+            <el-button slot="trigger" size="small" type="primary">Select file</el-button>
+            <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">Upload to server</el-button>
+            <el-button size="small" type="danger" @click="closeUploadDialog">Cancel</el-button>
+            <div class="el-upload__tip" slot="tip"><!-- Todo: Let other plugins hook in their component here --></div>
+        </el-upload>
     </div>
   </div>
 </template>
@@ -22,15 +30,22 @@
 <script>
 export default {
 
-  data () {
+  data() {
     return {
         uploadDialogVisible: false,
-        uploadPath: "/files/%2F" // Fixme: Allow for custom paths in filerepo
+        uploadPath: "/files/%2F",
+        errorHandler: this.uploadError,
+        selectionHandler: this.uploadChanged,
+        successHandler: this.uploadSuccess,
+        fileNameEnding: "",
+        fileListType: "text"
     }
   },
 
-  updated() {
-    console.log("[Upload-Dialog] Registered Handler", this.$store.state.upload.handler)
+  computed: {
+    handler: function () {
+      return this.$store.state.upload.handler
+    }
   },
 
   methods: {
@@ -41,6 +56,30 @@ export default {
     },
     uploadError(err, file, fileList) {
       console.warn("upload failed", err, file, fileList)
+    },
+    uploadChanged(file, fileList) {
+      console.log("[Upload Dialog] file selected for upload", file.raw)
+      let fileNameEnding = file.name.slice(file.name.lastIndexOf(".") + 1).toUpperCase()
+      let available = this.handler[file.raw.type.split("/")]
+      if (typeof available === "undefined") {
+        console.log("[Upload Dialog] no custom file upload handler registered for mimeType, checking for fileNameEnding...")
+        available = this.handler[fileNameEnding]
+        if (typeof available === "undefined") {
+            console.log("[Upload Dialog] Using standard upload handler, uploading to root directory of filerepository")
+        }
+      }
+      // ...
+      if (typeof available !== "undefined") {
+        console.log("[Upload Dialog] Hooking in custom file upload handler", available)
+        console.log("[Upload Dialog] Trying to register custom handler...")
+        this.errorHandler = available[0].error
+        this.successHandler = available[0].success
+        this.selectionHandler = available[0].selected
+        this.uploadPath = available[0].action
+      }
+    },
+    beforeUpload(file) {
+        // ### Todo: once a file was selected, subsequent files must have same file ending
     },
     openUploadDialog() {
       this.uploadDialogVisible = true
--- src/main/js/upload-dialog-store.js
+++ src/main/js/upload-dialog-store.js
@@ -7,11 +7,11 @@ const actions = {
   registerUploadHandler({state}, handler) {
     console.log("[Upload Dialog] Registering Upload Handler", handler, state)
     if (state.handler.hasOwnProperty(handler.mimeType)) {
-      console.log("[Upload Dialog] Appending handler for mimeType", handler.mimeType, "list => ", state.handler)
+      console.log("[Upload Dialog] Appending handler for mimeType (resp. fileEnding)", handler.mimeType, "list => ", state.handler)
       state.handler[handler.mimeType].push(handler)
     } else {
       state.handler[handler.mimeType] = [handler]
-      console.log("[Upload Dialog] Init handler for mimeType", handler.mimeType, "list =>", state.handler)
+      console.log("[Upload Dialog] Init handler for mimeType, (resp. fileEnding)", handler.mimeType, "list =>", state.handler)
     }
   }
 
